9c391337b602e5f9c2e48dde1b29b48f8c91d56b
fayman1 <fmuigai269@gmail.com>
2024-03-12 15:45:28 -0700

    7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
+    num = ord(c)
+    if num in range(97, 123):
+        return True
+    else:
+        return False
